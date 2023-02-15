3e8a876826e01a17e17ef091a82222ee0822369e
Hussien Mohamed <hussienmahba@gmail.com>
2023-02-15 19:16:04 +0200

    disable scroll when modal open

--- app.js
+++ app.js
@@ -1,61 +1,62 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add("show");
+      entry.target.classList.add('show');
     } else {
-      entry.target.classList.remove("show");
+      entry.target.classList.remove('show');
     }
   });
 });
 
-const hiddenElements = document.querySelectorAll(".hidden");
+const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
 // Code for the projects filter
-$(document).on("click", ".project-filter li", function () {
+$(document).on('click', '.project-filter li', function () {
   $(this)
-    .addClass("project-filter-active")
+    .addClass('project-filter-active')
     .siblings()
-    .removeClass("project-filter-active");
+    .removeClass('project-filter-active');
 });
 
 $(document).ready(function () {
-  $(".list").click(function () {
-    const value = $(this).attr("data-filter");
-    if (value == "all") {
-      $(".project-box").show("1000");
+  $('.list').click(function () {
+    const value = $(this).attr('data-filter');
+    if (value == 'all') {
+      $('.project-box').show('1000');
     } else {
-      $(".project-box")
-        .not("." + value)
-        .hide("1000");
-      $(".project-box")
-        .filter("." + value)
-        .show("1000");
+      $('.project-box')
+        .not('.' + value)
+        .hide('1000');
+      $('.project-box')
+        .filter('.' + value)
+        .show('1000');
     }
   });
 });
 
 // Modals code
-const openModalButtons = document.querySelectorAll("[data-modal-target]");
-const closeModalButtons = document.querySelectorAll("[data-close-button]");
-const overlay = document.getElementById("overlay");
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+const body = document.querySelector('body');
 
 openModalButtons.forEach((button) => {
-  button.addEventListener("click", () => {
+  button.addEventListener('click', () => {
     const modal = document.querySelector(button.dataset.modalTarget);
     openModal(modal);
   });
 });
 
 closeModalButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const modal = button.closest(".modal");
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
     closeModal(modal);
   });
 });
 
-overlay.addEventListener("click", () => {
-  const modals = document.querySelectorAll(".modal.active");
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active');
   modals.forEach((modal) => {
     closeModal(modal);
   });
@@ -63,12 +64,14 @@ overlay.addEventListener("click", () => {
 
 function openModal(modal) {
   if (modal == null) return;
-  modal.classList.add("active");
-  overlay.classList.add("active");
+  modal.classList.add('active');
+  overlay.classList.add('active');
+  body.style.overflow = 'hidden';
 }
 
 function closeModal(modal) {
   if (modal == null) return;
-  modal.classList.remove("active");
-  overlay.classList.remove("active");
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+  body.style.overflow = 'auto';
 }
